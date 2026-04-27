43c931daae6d80bb7a905db797e828ab19ac8e8b
Webforgeb <oumaymalh38@gmail.com>
2026-04-27 13:19:06 +0100

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function About() {
   return (
     <section className="about">
-      <img src="public/hotel.jpeg" alt="" />
+      <img src="/hotel.jpeg" alt="" />
 
       <div>
         <h2>À propos</h2>
